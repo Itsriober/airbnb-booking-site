2153a291eb55bb8da079f5e162f6180b66f3b113
Justine Harmon <jstnharmon02@gmail.com>
2025-06-05 11:05:50 +0300

    503 Page Update

---
+++ resources/views/frontend/template-1/auth/503.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>We'll be back soon!</title>
+    <style>
+        body { text-align: center; padding: 150px; font-family: Lato, sans-serif; }
+        h1 { font-size: 50px; }
+        body { font: 20px Helvetica, sans-serif; color: #333; }
+        article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+    </style>
+</head>
+<body>
+    <article>
+        <h1>We'll be back soon!</h1>
+        <div>
+            <p>Sorry for the inconvenience but we're performing some maintenance at the moment. If you need to you can always <a href="mailto:support@example.com">contact support</a>, otherwise we'll be back online shortly!</p>
+            <p>&mdash; The HarmoStays Team</p>
+        </div>
+    </article>
+</body>
+</html>
